5c558f940a4e90bbdb5119fd0f196673c48bb3f5
MuhammedSammir7 <mmss2012377@gmail.com>
2024-09-05 01:15:57 +0300

    creating authentication Storyboard and make the two view related to it

---
+++ El_DUKKANA/Modules/Authentication/View/SignInVC.swift
@@ -0,0 +1,40 @@
+//
+//  SignInVC.swift
+//  El_Dukkana
+//
+//  Created by ios on 03/09/2024.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var forgotPasswordBtn: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    @IBAction func loginBtnAction(_ sender: Any) {
+    }
+    @IBAction func forgotPasswordBtnAction(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
