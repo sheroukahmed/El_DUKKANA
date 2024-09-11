7c4d09674ddefbc0e8feaa26c3b0aab94833b96e
MuhammedSammir7 <mmss2012377@gmail.com>
2024-09-11 17:13:33 +0300

    k

--- El_DUKKANA/Modules/Authentication/View/SignInVC.swift
+++ El_DUKKANA/Modules/Authentication/View/SignInVC.swift
@@ -26,23 +26,11 @@ class SignInVC: UIViewController {
         self.dismiss(animated: true)
     }
     @IBAction func loginBtnAction(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "ProductDetailsStoryboard", bundle: nil)
-        let signUpVC = storyboard.instantiateViewController(identifier: "ProductDetailsVC")
-        signUpVC.modalPresentationStyle = .fullScreen
-        signUpVC.modalTransitionStyle = .crossDissolve
-       present(signUpVC, animated: true)
+        
     }
     @IBAction func forgotPasswordBtnAction(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
